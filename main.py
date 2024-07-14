77dd031d16ea5bac4216587e751dc9fecd665dd5
Mankifg <83959255+Mankifg@users.noreply.github.com>
2024-07-14 17:42:13 +0200

    some css + other stuf

--- main.py
+++ main.py
@@ -10,14 +10,36 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from typing import Optional
 import json
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
 
 from pyTwistyScrambler import scrambler333
 
 import magiccube
+
+import re
+
+import ast
+
+pattern = "(([UuDdLlRrFfBbMESxyz]|[UDLRFB]w)[2']?)+"
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 def up_face(alg):
+    
+    print("*"*10)
+    print(alg)
+    
+    #allowed = "UDLRFBudlrfbw2' "
+    allowed = "UDLRFBw2' "
+    
+    for a in alg:
+        if a not in allowed:
+            return "BBBBBBBBB"
+    
     cube = magiccube.Cube(
         3, "YYYYYYYYYRRRRRRRRRGGGGGGGGGOOOOOOOOOBBBBBBBBBWWWWWWWWW")
     
@@ -52,49 +74,113 @@ def list_of_scrambles_to_uface(scrambles):
 
 @app.get("/")
 async def root(request: Request):
-     
-    
     return templates.TemplateResponse("index.html", {"request": request,})
+
+def the_main(s,index):
+    a_el = 0
+    for a in s:
+        a_el = a_el + len(a)
+    print(a_el)
+    
+    
+    if s:
+        print(s)
+        print(type(s))
+        elements = ast.literal_eval(s)
+        #elements = json.loads(s)
+        print(elements)
+    else:
+        return False,"First you nead to generate scrambles.",None
+
+    if index >= len(elements) or index < 0:
+        index = 0  
+    el = elements[index]
+    
+    scrms = list_of_scrambles_to_uface(el)
+    
+    count = 0
+    for i in range(index):
+        count = count + len(elements[i])
+    print(count)
+    
+    nums = list(range(count+1,100))
+    comb = list(zip(el, scrms,nums))
+    
+    return True,comb,index
 
 
 @app.get("/scramble", response_class=HTMLResponse)
 async def scramble(request: Request, s: Optional[str] = Cookie(None)):
     index = int(request.query_params.get('index', 0))  # Get index from query params, default to 0
+
+    succes,data,index = the_main(s,index)
     
-    elements = json.loads(s)
-    if index > len(elements) or index < 0:
-        index = 0  
-    el = elements[index]
-    scrms = list_of_scrambles_to_uface(el)
-    comb = list(zip(el, scrms,list(range(1,len(scrms)+1))))
+    if not succes:
+        response = templates.TemplateResponse("index.html", {"request": request, "error": data})
+        return response
     
     
-    return templates.TemplateResponse("scramble.html", {"request": request, "comb": comb, "index": index,})
+    return templates.TemplateResponse("scramble.html", {"request": request, "comb": data, "index": index})
 
 @app.post("/set_index/{new_index}", response_class=HTMLResponse)
 async def set_index(request: Request, new_index: int, s: Optional[str] = Cookie(None)):
    
     index = new_index
-   
-    elements = json.loads(s)
-    if index > len(elements) or index < 0:
-        new_index = 0  
-    el = elements[new_index]
-    scrms = list_of_scrambles_to_uface(el)    
-    comb = list(zip(el, scrms,list(range(1,len(scrms)+1))))
     
+    succes,data,index = the_main(s,index)
+    
+    if not succes:
+        response = templates.TemplateResponse("index.html", {"request": request, "error": data})
+        return response
+    
+    return templates.TemplateResponse("scramble.html", {"request": request, "comb": data, "index": index})
 
-    response = templates.TemplateResponse("scramble.html", {"request": request, "comb": comb, "index": new_index,})
-    response.set_cookie(key="s", value=json.dumps(elements))
+
+@app.get("/userscrambles")
+async def read_root(request: Request):
+    response = templates.TemplateResponse("userScrambles.html", {"request": request})
     return response
 
-@app.get("/random")
-async def read_root(request: Request):
-    
-    scr = scrambler333.get_3BLD_scramble()
-    
-    return {"scr":scr}
 
+@app.post("/userscrambleget")
+async def handle_form(response:Response,input_text: str = Form(...)):
+
+    input_text = input_text.replace("\r", "")
+    input_text = input_text.replace("", "")
+    
+    inp = input_text.split("\n")
+    
+    print(inp)
+    
+    
+    cubes = len(inp)
+    
+    width, height, rem = scale(cubes)
+    
+    scrambles = []
+    i = 0
+    for _ in range(width):
+        n = []
+        for _ in range(height):
+            c = inp[i]
+            i = i + 1
+            n.append(c)
+        scrambles.append(n)
+    
+    extra = []
+    while i < len(inp):
+        c = inp[i]
+        i = i+1
+        extra.append(c)
+    
+    if len(extra) != 0:
+        scrambles.append(extra)
+    
+    print(scrambles)
+    response.set_cookie(key="s", value=scrambles, httponly=True)
+
+
+    return {"output": "Finished"}
 
 @app.post("/save_scr")
 async def save_scr(response: Response,cubes: int = 2):
@@ -113,8 +199,6 @@ async def save_scr(response: Response,cubes: int = 2):
     for _ in range(reaminder):
         extra.append(scrambler333.get_3BLD_scramble())
     scrambles.append(extra)
-    
-    print(scrambles)
     
     response.set_cookie(key="s", value=scrambles, httponly=True)
     
